94366a5aba071fd4d500a5b62fc77e1a5cd8e41a
itc-s23020 <s23020@std.it-college.ac.jp>
2024-01-16 07:13:58 +0000

    ch9.1fin

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -12,6 +12,8 @@ import {
 import ConvertBody from 'components/convert-body'
 import PostCategories from 'components/post-categories'
 import Image from 'next/image'
+import { getPlaiceholder } from 'plaiceholder'
+import { eyecatchLocal } from 'lib/constants'
 
 const Schedule = ({
   title,
@@ -42,6 +44,8 @@ const Schedule = ({
             height={eyecatch.height}
             sizes='(min-width: 1152px) 1152px, 100vw'
             priority
+            placeholder='blur'
+            blurDateURL={eyecatch.blurDateURL}
           />
         </figure>
 
@@ -62,21 +66,25 @@ const Schedule = ({
 export default Schedule
 
 export async function getStaticProps () {
-  const slug = 'schedule'
+  const slug = 'micro'
 
   const post = await getPostBySlug(slug)
 
   const description = extractText(post.content)
+
+  const eyecatch = post.eyecatch ?? eyecatchLocal
+
+  const { base64 } = await getPlaiceholder(eyecatch.url)
+  eyecatch.blurDateURL = base64
 
   return {
     props: {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
-      eyecatch: post.eyecatch,
+      eyecatch,
       categories: post.categories,
       description
-
     }
   }
 }
